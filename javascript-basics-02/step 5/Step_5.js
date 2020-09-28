befe970a513ce77d2bf3eb11b2bf23e5af81584b
Marwa Al Kirdy <marwaalkirdy96@gmail.com>
2020-09-28 13:17:41 +0300

    Completed javascript-basics-02 Step 5

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,6 @@
+document.querySelector("#image1").addEventListener("mouseover", function() {
+    this.src = "images/image1_2.jpg";
+    document.querySelector("#image2").src = "images/image2_2.jpg";
+    document.querySelector("#image3").src = "images/image3_2.jpg";
+    document.querySelector("#image4").src = "images/image4_2.jpg";
+    document.querySelector("#image5").src = "images/image5_2.jpg";});
