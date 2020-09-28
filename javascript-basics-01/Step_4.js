7bc886cddf44eb3efed35cb8a0f3e5b8a20b19fe
Marwa Al Kirdy <marwaalkirdy96@gmail.com>
2020-09-28 11:01:06 +0300

    Completed javascript-basics-01 Step 4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("Please enter yout name: ");
+var surname = prompt("Please enter yout surname: ");
+var city = prompt("Please enter yout city: ");
+alert("Hello, " + name + " " + surname + " from " + city);
