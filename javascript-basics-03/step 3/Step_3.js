e1b9583a415e40db10fdb0a7bcca419145b4973d
Marwa Al Kirdy <marwaalkirdy96@gmail.com>
2020-09-28 17:40:30 +0300

    Completed javascript-basics-03 Step 3

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,11 @@
+document.querySelector(".green").addEventListener("click", function () {
+    document.querySelector("#text").style.color = "green";
+});
+
+document.querySelector(".red").addEventListener("click", function () {
+    document.querySelector("#text").style.color = "red";
+});
+
+document.querySelector(".blue").addEventListener("click", function () {
+    document.querySelector("#text").style.color = "blue";
+});
